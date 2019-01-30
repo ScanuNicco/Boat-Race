71461a6369a730191afee4cdebd69c151eed6354
zorbulator <zorbulator@gmail.com>
2019-01-30 13:09:08 -0800

    Removed loop
    it was causing too many problems

--- practice.js
+++ practice.js
@@ -854,13 +854,13 @@ function animate() {
 			    
 		    collisionResults = checkCollision();
 			    
-		    while (collisionResults != null) {
-			    collisionResults = checkCollision();
+		    //while (collisionResults != null) {
+			 //   collisionResults = checkCollision();
 			    controls.getObject().position.x += collisionResults.face.normal.x * COLLISION_CORRECTION_INCREMENT;
 			    controls.getObject().position.z -= collisionResults.face.normal.y * COLLISION_CORRECTION_INCREMENT; //Don't know why this works but it does, probably related to rotating the track
-		    	    boat.position.x = controls.getObject().position.x;
-        		    boat.position.z = controls.getObject().position.z;
-		    }
+		    	//    boat.position.x = controls.getObject().position.x;
+        		//    boat.position.z = controls.getObject().position.z;
+		    //}
 		    } catch(e) {
 			    alert("Error with collision: " + e.message);
 		    }
